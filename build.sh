086d5d2bade2d0ceea49da02c0de767269797757
prashantpaddune <elonpras@my.smccd.edu>
2018-06-02 10:54:36 +0530

    Add build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+DTS=arch/arm64/boot/dts
+RDIR=$(pwd)
+# GCC
+export CROSS_COMPILE=/home/prashantp/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+# Cleanup
+make clean && make mrproper
+# A510F NN Defcon
+make samfr_a5xelte_00_defconfig
+make exynos7580-a5xelte_eur_open_00.dtb exynos7580-a5xelte_eur_open_01.dtb exynos7580-a5xelte_eur_open_02.dtb exynos7580-a5xelte_eur_open_03.dtb exynos7580-a5xelte_eur_open_08.dtb exynos7580-a5xelte_eur_open_09.dtb
+# Make zImage
+make ARCH=arm64 -j5
+# Make DT.img
+./tools/dtbtool -o ./boot.img-dtb -v -s 2048 -p ./scripts/dtc/ $DTS/
+# Cleaup
+rm -rf $DTS/.*.tmp
+rm -rf $DTS/.*.cmd
+rm -rf $DTS/*.dtb
--- build_kernel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export ARCH=arm64
-export CROSS_COMPILE=/home/samfr/gcc/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-
-make mrproper
-make samfr_a5xelte_00_defconfig
-
-make -j4;
